38843e1a19237eaf46fc1dd255198139620d8e85
safetyWhat <137813061+safetyWhat@users.noreply.github.com>
2023-08-16 17:17:02 -0400

    Finish required aspects of porject.

--- README.md
+++ README.md
@@ -14,18 +14,23 @@ Create functions for:
 
 Create a function that takes num1 and num2 and operates them based on the operator selected. //done
 
-1. When a number is clicked it gets added to the end of the displayed character.
-    build an array using push. Once the operator is clicked the array is converted to number.
+1. When a number is clicked it gets added to the end of the displayed character. //done
+    build an array using push. Once the operator is clicked the array is converted to number. //done
 2. When an operator( +, -, x, / ) is clicked:
-    a. the number in the display is logged as num1.
-    b. the clicked operator is logged as operator.
-3. When a number is clicked it gets added to the end of the displayed character.
+    a. the number in the display is logged as num1. //done
+    b. the clicked operator is logged as operator. //done
+3. When a number is clicked it gets added to the end of the displayed character. //done
 4. When equal is clicked:
-    a. the number in the display is logged as num2.
-    b. the operate function is run.
+    a. the number in the display is logged as num2. //done
+    b. the operate function is run. //done
 Alts:
     a. at any point if the clear(c) button is clicked, the display is cleared.
         1. Run an if loop if display has number => clr //num2 will not be assigned yet.
         2. else if operator is not empty => clr
         3. else if num1 is not empty => clr
     b. at any point is the decimal(.) is clicked it adds a decimal to the displayed characters.
+
+Add decimal button
+Add "big" clear button (reset/refresh)
+Add negative option (?)
+Add divs to left side of display to show num1 and operator values once input. (?)
--- script.js
+++ script.js
@@ -3,44 +3,59 @@ let num2 = 0
 let operator = "";
 let numArray = [];
 let solution = 0
+let display = document.getElementById("display");
+display.textContent = 0;
 
 function buildNumber(numArray, digit) {
-    numArray.push(digit)
-    //console.log(numArray)
-    let newNum = numArray.join('');
-    //console.log(newNum);
-    let display = document.getElementById("display");
+    numArray.push(digit) //adds clicked number to array
+    let newNum = numArray.join(''); //converts array to complete number
     display.textContent = newNum;
-    //return newNum;
+}
+
+function empty() {
+    num1 = 0;
+    num2 = 0;
+    operator = "";
+    numArray = [];
+    solution = 0;
+    display.textContent = 0;
+    console.log(num1,num2,operator,numArray,solution,display.textContent);
 }
 
 function convertNum1(oper) {
-    let display = document.getElementById("display");
+    if (num1 > 0 || num1 < 0) {
+        operate();
+        display.textContent = solution;
+        num1 = solution;
+        operator = oper;
+    } else {
     num1 = display.textContent;
-    numArray = [];
-    display.textContent = '';
-    console.log(typeof +num1);
+    numArray = []; //clears the memory so a new number can be input
+    //display.textContent = '';
     operator = oper;
-}
-
-function checkNum1() {
-    console.log(num1, operator);
+    }
 }
 
 let operate = function() {
-    let display = document.getElementById("display");
     num2 = display.textContent;
-    numArray = [];
-    if (operator === "addition") {
+    numArray = []; //clears the memory so a new number can be input. Allows new calculation without page refresh.
+    if (num2 === '0' && operator === 'divide') { //dividing by 0 creates infinity. We don't do that.
+        return display.textContent = 'Umm, NO!';
+    }else if (operator === "addition") {
         addition(+num1, +num2);
     } else if (operator === 'subtract') {
         subtract(num1, num2);
     } else if (operator === 'multiply') {
         multiply(num1, num2);
-    } else {
+    } else { //all that's left is divide
         divide(num1, num2);
     }
+    console.log(solution)
+    solution = +solution.toFixed(2);
     display.textContent = solution;
+    num1 = 0;
+    num2 = 0;
+    operator = '';
 }
 
 let addition = function(a, b) {
